96ffa632a59846632d4965aa17b433b452854a00
Vicente <vicho0202@gmail.com>
2023-05-02 11:07:11 -0400

    Initial commit

---
+++ YND Mover/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CodeWalker.GameFiles;
+using FolderBrowserEx;
+using Microsoft.Win32;
+using SharpDX;
+using Xceed.Wpf.Toolkit;
+using SharpDX.Mathematics;
+using MessageBox = Xceed.Wpf.Toolkit.MessageBox;
+using Path = System.IO.Path;
+
+namespace YND_Mover
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<string> YNDFiles = new();
+        private string outputpath;
+        
+        public MainWindow()
+        {
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+            InitializeComponent();
+        }
+
+        private void BtnSelectYND_OnClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new()
+            {
+                Multiselect = true,
+                DefaultExt = ".ynd",
+                Filter = "YND Files (*.ynd)|*.ynd",
+                Title = "Select your YND file(s)"
+            };
+
+            if (ofd.ShowDialog() != true) return;
+            YNDFiles = ofd.FileNames.ToList();
+            MessageBox.Show($"Selected {YNDFiles.Count} YND files", "YND Mover", MessageBoxButton.OK, MessageBoxImage.Information);
+            
+        }
+
+        private void BtnMovePaths_OnClick(object sender, RoutedEventArgs e)
+        {
+            if(YNDFiles.Count == 0 && string.IsNullOrEmpty(outputpath)) return;
+            var nodes = PathUtils.GetNodesFromYNDs(YNDFiles);
+            PathUtils.MoveAllNodes(nodes, new Vector3((float)OffsetX.Value, (float)OffsetY.Value, (float)OffsetZ.Value));
+            PathUtils.UpdateAreaIDfromNodes(nodes);
+            PathUtils.GenerateYNDperArea(nodes, outputpath);
+            MessageBox.Show("Done!", "YND Mover", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void BtnOutput_OnClick(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog dlg = new()
+            {
+                Title = "Select your output folder",
+                InitialFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+                AllowMultiSelect = false
+            };
+            if (dlg.ShowDialog() != System.Windows.Forms.DialogResult.OK) return;
+            outputpath = dlg.SelectedFolder;
+            MessageBox.Show($"Output folder set to {Path.GetDirectoryName(outputpath)}", "YND Mover", MessageBoxButton.OK, MessageBoxImage.Information);
+
+        }
+    }
+}
---
+++ YND Mover/PathUtils.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Controls.Ribbon;
+using CodeWalker.GameFiles;
+using SharpDX;
+
+namespace YND_Mover;
+
+public class PathUtils
+{
+    public static int GetAreaIDFromNodePosition(Vector3 position)
+    {
+        int AreaID = 0;
+        int Result = 0;
+        float Xpos = -8192f;
+        float Ypos = -8192f;
+
+        for (int i = 0; i < 32; i++)
+        {
+            for (int j = 0; j < 32; j++)
+            {
+                float xmin = Xpos;
+                float xmax = Xpos + 512f;
+                float ymin = Ypos;
+                float ymax = Ypos + 512f;
+
+                float node_x = position.X;
+                float node_y = position.Y;
+                bool v = ymin < node_y && node_y < ymax;
+                bool v1 = xmin < node_x && node_x < xmax;
+
+                if (v1 && v)
+                {
+                    Result = AreaID;
+                }
+                AreaID += 1;
+                Xpos += 512f;
+            }
+            Ypos += 512f;
+            Xpos = -8192f;
+        }
+
+        return Result;
+    }
+
+    public static List<YndNode> GetNodesFromYNDs(List<string> yndfiles)
+    {
+        List<YndNode> nodes = new();    
+        foreach (var yndfile in yndfiles)
+        {
+            YndFile yf = new();
+            yf.Load(File.ReadAllBytes(yndfile));
+            nodes.AddRange(yf.Nodes.ToList());
+        }
+        return nodes;
+    }
+    
+    public static void MoveAllNodes(List<YndNode> nodes, Vector3 position)
+    {
+        foreach (var node in nodes)
+        {
+            node.Position += position;
+        }
+    }
+    
+    public static void UpdateAreaIDfromNodes(List<YndNode> nodes)
+    {
+        foreach (var node in nodes)
+        {
+            node.AreaID = Convert.ToUInt16(GetAreaIDFromNodePosition(node.Position));
+        }
+    }
+    
+    public static void GenerateYNDperArea(List<YndNode> nodes, string outputdir)
+    {
+        var groupedNodes = nodes.GroupBy(node => node.AreaID);
+        
+        foreach (var group in groupedNodes)
+        {
+            YndFile yf = new()
+            {
+                Nodes = group.ToArray(),
+                AreaID = group.Key,
+                CellX = group.Key % 32,
+                CellY = group.Key / 32,
+                HasChanged = true,
+                Name = $"nodes{group.Key}"
+                
+            };
+            yf.UpdateAllNodePositions();
+            yf.BuildBVH();
+            var filename = $"nodes{yf.AreaID}.ynd";
+            byte[] newBytes = yf.Save();
+            File.WriteAllBytes(Path.Combine(outputdir, $"{yf.Name}.ynd"), newBytes);
+        }
+    }
+
+}
